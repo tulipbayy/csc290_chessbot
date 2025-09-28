5d9b3a7789b006eb64efc955c84def54521d34cf
HalaMaloul <hmaloul@smith.edu>
2025-09-28 18:55:01 -0400

    Banner

---
+++ chessbot.py
@@ -0,0 +1,12 @@
+
+from datetime import datetime
+import random
+import chess
+
+BANNER = """
+=====================================================
+             CS 290 Chess Bot Version 0.1
+=====================================================
+"""
+
+
